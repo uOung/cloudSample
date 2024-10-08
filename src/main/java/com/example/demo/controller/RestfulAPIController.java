3ee7adf13414795392a5f556fc5c893a9aabcf12
uOung <youyoung0703@gmail.com>
2024-10-08 17:54:57 +0900

    updated Restful_API

--- src/main/java/com/example/demo/controller/RestfulAPIController.java
+++ src/main/java/com/example/demo/controller/RestfulAPIController.java
@@ -1,2 +1,13 @@
-package com.example.demo.controller;public class RestfulAPIController {
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestfulAPIController {
+
+    @GetMapping("/api/v1/user")
+    public String home() {
+        return "82240977";
+    }
 }
